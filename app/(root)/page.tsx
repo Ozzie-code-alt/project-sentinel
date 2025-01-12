88eca667a349c0ecfba70e6384549130161bde7e
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2025-01-12 23:23:31 +0800

    feat: add Book Cover and Overview Components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,11 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-
+import { sampleBooks } from "@/constants";
 
 export default function Home() {
   return (
     <>
-      <BookOverview />
+      <BookOverview {...sampleBooks[0]}/>
       <BookList />
     </>
   );
