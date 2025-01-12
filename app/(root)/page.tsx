ec952eedc413989979e9eb7510f49513cbd661b6
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2025-01-12 13:49:26 +0800

    chore: add constants

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,6 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
